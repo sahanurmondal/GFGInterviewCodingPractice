4fcec5ff5695482d9af2aa62736227c5f9cd7ea9
SahanurM <sahanur.mondal@talentica.com>
2018-11-14 18:32:14 +0530

    commit

---
+++ src/com/gfg/array/Count_possible_triangles.java
@@ -0,0 +1,26 @@
+package com.gfg.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Count_possible_triangles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        long n=0;
+        long x=0,y=0;
+        StringTokenizer tk;
+        while(t-->0)
+        {
+            n=Long.parseLong(br.readLine());
+            tk = new StringTokenizer(br.readLine());
+            
+
+            sb.append("").append("\n");
+        }
+        System.out.print(sb);
+    }
+}
