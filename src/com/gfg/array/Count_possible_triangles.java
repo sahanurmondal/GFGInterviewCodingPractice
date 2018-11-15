ad446d8f13260b9695911c02f18850e7fa3881b9
SahanurM <sahanur.mondal@talentica.com>
2018-11-15 15:53:44 +0530

    commit

--- src/com/gfg/array/Count_possible_triangles.java
+++ src/com/gfg/array/Count_possible_triangles.java
@@ -3,6 +3,7 @@ package com.gfg.array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Count_possible_triangles {
@@ -10,16 +11,34 @@ public class Count_possible_triangles {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
-        long n=0;
-        long x=0,y=0;
+        int n=0;
+        //int i,j,k;
+        long c;
+        //long x=0,y=0;
         StringTokenizer tk;
+        int[] arr;
         while(t-->0)
         {
-            n=Long.parseLong(br.readLine());
+            n=Integer.parseInt(br.readLine());
             tk = new StringTokenizer(br.readLine());
-            
-
-            sb.append("").append("\n");
+            arr=new int[n];
+            for (int x = 0; x <n ; x++) {
+                arr[x]= Integer.parseInt(tk.nextToken());
+            }
+            Arrays.sort(arr);
+            //i=0;j=0;k=0;
+            c=0;
+            for (int i = 0; i < n-2; i++) {
+                int k = i+2;
+                for (int j = i+1; j < n; j++) {
+                    while (k < n && arr[i]+arr[j] > arr[k]) {
+                        ++k;
+                    }
+                    c += (k-j-1);
+                }
+            }
+            //sb.append(binomialCoefficient(n,3)-c).append("\n");
+            sb.append(c).append("\n");
         }
         System.out.print(sb);
     }
